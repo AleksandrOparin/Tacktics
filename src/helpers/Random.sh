42d325e6a3e7c4b2c537e918788e73eb706c79d4
Александр <al.oparin@vk.team>
2024-04-22 10:37:23 +0300

    Delete comment

--- src/helpers/Random.sh
+++ src/helpers/Random.sh
@@ -5,7 +5,6 @@ generateRandomSequence() {
     
     local sequence
     sequence=$(openssl rand -base64 48 | tr -dc 'a-zA-Z0-9' | head -c"$length")
-    # sequence=$(mcookie | tr -dc 'a-zA-Z0-9' | head -c$length)
     
     echo "$sequence"
 }
